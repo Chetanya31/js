b1aa9f3d1132e73ae6911e2e586d5c6409619cc9
Chetanya31 <129482017+Chetanya31@users.noreply.github.com>
2024-02-09 03:14:57 +0000

    data conversion

---
+++ 1bs/dtconv.js
@@ -0,0 +1,15 @@
+let score=35
+if ( typeof (score) == "number"){
+    console.log("nice");
+
+}
+
+let score2 = "33a"
+
+console.log((Number(score2)))
+
+console.log((string(score)))
+
+
+// when converting  no in string it can be done but if you try to convert string to numbers or undefined than
+//  it will convert but the output would be zero or Nan. 
---
+++ 1bs/op.js
@@ -0,0 +1,28 @@
+console.log(3+2)
+console.log(3+"2"+2)
+console.log("3"+2)
+console.log("3"+2+2)
+console.log(2+2+"3")
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let c=20
+c++
+console.log(c);
+
+
+//  simple check
+
+console.log(2=="2")
+console.log(2>"3")
+console.log("3"==3)
+//  in this data type converted  accordingly
+
+
+//  (===) this is strict check in this this data type is also checked
+
+console.log(2==="2")
+console.log(3==="3")
+
