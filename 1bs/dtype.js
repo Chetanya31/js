aeec2db72169d5dd1659089b13cd4401432f43d3
Chetanya31 <129482017+Chetanya31@users.noreply.github.com>
2024-02-09 00:55:55 +0000

    data types

---
+++ 1bs/dtype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer verson
+
+// alert ("hello")
+// alert(3+3) we are using node js,not browser
+console.log(3
+    +
+   3) //  code readability should be good
+
+
+console.log("hi chetan")
+
+let name="chetanya"
+let age=23 
+let isage=false
+let state; 
+
+// number - 1,string="",boolean-true,false,null=>  standalone value, undefined=>,symbol=> unique
+
+
+
+// object
+
+console.log(typeof null)// object
