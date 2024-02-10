5727866ce3e21743da301139fc0bf16ce761bae4
Chetanya31 <129482017+Chetanya31@users.noreply.github.com>
2024-02-10 20:12:45 +0000

    arr

--- 1bs/array.js
+++ 1bs/array.js
@@ -10,5 +10,24 @@ arr2.push(9)
 
 // pop
 
-// arr2.pop()
+arr2.pop()
+console.log(arr2)
 
+arr2.pop()
+console.log(arr2)
+const newarr=arr2.join("")
+console.log(newarr)
+
+// slice spice
+
+let nu=[1,23,45,5,6]
+console.log("A ",nu)
+const na=nu.slice(1,3)
+
+console.log(na)
+
+console.log("B ",nu)
+
+const ng=nu.splice(1,3)
+console.log("C ",nu)
+console.log(ng);
